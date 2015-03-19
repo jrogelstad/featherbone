ac2a12945b741d9b1fa23004f3067f60b8536244
John Rogelstad <john@rogelstad.net>
2015-03-19 19:26:37 -0400

    Simplify

--- pg/init.sql
+++ pg/init.sql
@@ -17,10 +17,10 @@
 create or replace function fp.init() returns void as $$
   return (function () {
 
-    var _post,
+    var _add,
+      _get,
       _patch,
-      _delete,
-      _get;
+      _remove;
 
     // ..........................................................
     // NATIVE
@@ -65,137 +65,6 @@ create or replace function fp.init() returns void as $$
     plv8.FP = FP = {};
 
     /**
-      Create or update a persistence class. This function is idempotent.
-
-      Example payload:
-          {
-             "nameSpace": "FP",
-             "className": "Contact",
-             "description": "Contact data about a person",
-             "properties": [
-               {
-                 "action": "add",
-                 "name": "fullName",
-                 "description": "Full name",
-                 "type": "String",
-                 "isRequired": true,
-                 "defaultValue": ""
-               },
-               {
-                 "name": "birthDate",
-                 "description": "Birth date",
-                 "type": "Date"
-               },
-               {
-                 "name": "isMarried",
-                 "description": "Marriage status",
-                 "type": "Boolean",
-                 "isRequired": true
-               },
-               {
-                 "name": "dependents",
-                 "description": "Number of dependents",
-                 "type": "Number",
-                 "isRequired": true,
-                 "defaultValue": 0
-               }
-             ]
-          }
- 
-     * @param {Object} Class specification payload.
-     * @return {String}
-    */
-    FP.saveClass = function (obj) {
-      obj = obj || {};
-
-      var schema = (obj.nameSpace || 'fp').toSnakeCase(),
-        table = obj.className ? obj.className.toSnakeCase() : false,
-        inheritSchema = (obj.inherits ? obj.inherits.ere() || "fp" : "fp").toSnakeCase(),
-        inheritTable = (obj.inherits ? obj.inherits.hind() : 'object').toSnakeCase(),
-        sql = "select * from pg_tables where schemaname = $1 and tablename = $2;",
-        sqlChk = "select * " +
-         "from pg_class c, pg_namespace n, pg_attribute a, pg_type t " +
-         "where c.relname = $1 " +
-         " and n.nspname = $2 " +
-         " and a.attname = $3 " +
-         " and n.oid = c.relnamespace " +
-         " and a.attnum > 0 " +
-         " and a.attrelid = c.oid " +
-         " and a.atttypid = t.oid; ",
-        args = [schema, table, table + "_pkey", table + "_guid_key", inheritSchema, inheritTable],
-        actions = ["add", "drop"],
-        types = {
-          Object: "json", 
-          Array: "json", 
-          String: "text", 
-          Number: "numeric", 
-          Date: "timestamp with time zone",
-          Boolean: "boolean"
-        },
-        result = true,
-        found,
-        i;
-
-      if (!table) { return false };
-
-      /** Edit table **/
-      if (!plv8.execute(sql, [schema, table]).length) {
-        sql = FP.formatSql("create table %I.%I(constraint %I primary key (id), constraint %I unique (guid)) inherits (%I.%I)", args);
-        plv8.execute(sql);
-      }
-
-      if (obj.description) { 
-        sql = FP.formatSql("comment on table %I.%I is %L;", [schema, table, obj.description]);
-        plv8.execute(sql);
-      }
-
-      /** Edit columns **/
-       for (i = 0; i < obj.properties.length; i++) {
-        var prop = obj.properties[i],
-          action = prop.action || "add",
-          type = prop.type,
-          name = prop.name ? prop.name.toSnakeCase() : false,
-          args = [schema, table, action];
-
-        if (!name || actions.indexOf(action) === -1) {
-          result = false;
-          break;
-        }
-
-        args.push(name);
-        found = plv8.execute(sqlChk, [table, schema, name]).length;
-
-        /** Add to this switch to add support for more alter actions in the future**/
-        switch (action)
-        {
-        case "add":
-          if (Object.keys(types).indexOf(type) === -1) {
-            result = false;
-          } else {
-            if (!found) {
-              sql += FP.formatSql("alter table %I.%I %I column %I " + types[type], args);
-              sql += prop.isRequired ? " not null;" : ";";
-            }
-            if (prop.description) {
-              sql += FP.formatSql("comment on column %I.%I.%I is %L;", [schema, table, name, prop.description]);
-            }
-          }
-          break;
-        case "drop":
-          if (found) {
-            sql += FP.formatSql("alter table %I.%I %I column if exists %I;", args);
-          }
-          break;
-        }
-        if (!result) { break }
-      }
-
-      if (result) { plv8.execute(sql); }
-
-      return true;
-    };
-
-    /**
       Return a universally unique identifier.
 
       From http://stackoverflow.com/a/8809472/251019
@@ -221,14 +90,13 @@ create or replace function fp.init() returns void as $$
     FP.deleteClass = function (obj) {
       obj = obj || {};
 
-      var schema = (obj.nameSpace || 'fp').toSnakeCase(),
-        table = obj.className ? obj.className.toSnakeCase() : false,
-        sql = "select * from pg_tables where schemaname = $1 and tablename = $2;",
-        args = [schema, table];
+      var table = obj.className ? obj.className.toSnakeCase() : false,
+        sql = "select * from pg_tables where schemaname = 'fp' and tablename = $1;",
+        args = [table];
 
       if (!table || !plv8.execute(sql, args).length) { return false };
 
-      sql = FP.formatSql("drop table %I.%I", args);
+      sql = FP.formatSql("drop table fp.%I", args);
       plv8.execute(sql);
       
       return true;
@@ -272,7 +140,7 @@ create or replace function fp.init() returns void as $$
              "nameSpace": "FP",
              "className": "Contact",
              "action": "POST",
-             "value": {
+             "data": {
                "guid": "dc9d03f9-a539-4eca-f008-1178f19f56ad",
                "created": "2015-04-26T12:57:57.896Z",
                "createdBy": "admin",
@@ -291,13 +159,13 @@ create or replace function fp.init() returns void as $$
       switch (obj.action)
       {
       case "POST":
-        return _post(obj);
+        return _add(obj);
         break;
       case "PATCH":
-        return false;
+        return _patch(obj);
         break;
       case "DELETE":
-        return _delete(obj);
+        return _remove(obj);
         break;
       case "GET":
         return false;
@@ -305,16 +173,141 @@ create or replace function fp.init() returns void as $$
       }
     };
 
+    /**
+      Create or update a persistence class. This function is idempotent.
+
+      Example payload:
+          {
+             "nameSpace": "FP",
+             "className": "Contact",
+             "description": "Contact data about a person",
+             "properties": [
+               {
+                 "action": "add",
+                 "name": "fullName",
+                 "description": "Full name",
+                 "type": "String",
+                 "isRequired": true
+               },
+               {
+                 "name": "birthDate",
+                 "description": "Birth date",
+                 "type": "Date"
+               },
+               {
+                 "name": "isMarried",
+                 "description": "Marriage status",
+                 "type": "Boolean",
+                 "isRequired": true
+               },
+               {
+                 "name": "dependents",
+                 "description": "Number of dependents",
+                 "type": "Number",
+                 "isRequired": true
+               }
+             ]
+          }
+ 
+     * @param {Object} Class specification payload.
+     * @return {String}
+    */
+    FP.saveClass = function (obj) {
+      obj = obj || {};
+
+      var table = obj.className ? obj.className.toSnakeCase() : false,
+        inheritTable = (obj.inherits ? obj.inherits.hind() : 'object').toSnakeCase(),
+        sql = "select * from pg_tables where schemaname = 'fp' and tablename = $1;",
+        sqlChk = "select * " +
+         "from pg_class c, pg_namespace n, pg_attribute a, pg_type t " +
+         "where c.relname = $1 " +
+         " and n.nspname = 'fp' " +
+         " and a.attname = $2 " +
+         " and n.oid = c.relnamespace " +
+         " and a.attnum > 0 " +
+         " and a.attrelid = c.oid " +
+         " and a.atttypid = t.oid; ",
+        args = [table, table + "_pkey", table + "_guid_key", inheritTable],
+        actions = ["add", "drop"],
+        types = {
+          Object: "json", 
+          Array: "json", 
+          String: "text", 
+          Number: "numeric", 
+          Date: "timestamp with time zone",
+          Boolean: "boolean"
+        },
+        result = true,
+        found,
+        i;
+
+      if (!table) { return false };
+
+      /** Edit table **/
+      if (!plv8.execute(sql, [table]).length) {
+        sql = FP.formatSql("create table fp.%I(constraint %I primary key (id), constraint %I unique (guid)) inherits (fp.%I)", args);
+        plv8.execute(sql);
+      }
+
+      if (obj.description) { 
+        sql = FP.formatSql("comment on table fp.%I is %L;", [table, obj.description]);
+        plv8.execute(sql);
+      }
+
+      /** Edit columns **/
+      for (i = 0; i < obj.properties.length; i++) {
+        var prop = obj.properties[i],
+          action = prop.action || "add",
+          type = prop.type,
+          name = prop.name ? prop.name.toSnakeCase() : false,
+          args = [table, action];
+
+        if (!name || actions.indexOf(action) === -1) {
+          result = false;
+          break;
+        }
+
+        args.push(name);
+        found = plv8.execute(sqlChk, [table, name]).length;
+
+        /** Add to this switch to add support for more alter actions in the future**/
+        switch (action)
+        {
+        case "add":
+          if (Object.keys(types).indexOf(type) === -1) {
+            result = false;
+          } else {
+            if (!found) {
+              sql += FP.formatSql("alter table fp.%I %I column %I " + types[type], args);
+              sql += prop.isRequired ? " not null;" : ";";
+            }
+            if (prop.description) {
+              sql += FP.formatSql("comment on column fp.%I.%I is %L;", [table, name, prop.description]);
+            }
+          }
+          break;
+        case "drop":
+          if (found) {
+            sql += FP.formatSql("alter table fp.%I %I column if exists %I;", args);
+          }
+          break;
+        }
+        if (!result) { break }
+      }
+
+      if (result) { plv8.execute(sql); }
+
+      return true;
+    };
+
     // ..........................................................
     // Private
     //
 
     /** private */
-    _post = function (obj) {
-      var schema = (obj.nameSpace || 'fp').toSnakeCase(),
-        table = obj.className ? obj.className.toSnakeCase() : false,
-        keys = Object.keys(obj.value),
-        args = [schema, table],
+    _add = function (obj) {
+      var keys = Object.keys(obj.data),
+        args = [obj.className ? obj.className.toSnakeCase() : false],
         tokens = [],
         params = [],
         values = [],
@@ -325,10 +318,10 @@ create or replace function fp.init() returns void as $$
         args.push(keys[i].toSnakeCase());
         tokens.push("%I");
         params.push("$" + (i + 1));
-        values.push(obj.value[keys[i]]);
+        values.push(obj.data[keys[i]]);
       }
 
-      sql = FP.formatSql("insert into %I.%I (" + tokens.toString(",") + ") values (" + params.toString(",") + ") returning *;", args);
+      sql = FP.formatSql("insert into fp.%I (" + tokens.toString(",") + ") values (" + params.toString(",") + ") returning *;", args);
       result = plv8.execute(sql, values)[0];
       delete result.id;
       
@@ -336,16 +329,34 @@ create or replace function fp.init() returns void as $$
     };
 
     /** private */
-    _delete = function (obj) {
-      var schema = (obj.nameSpace || 'fp').toSnakeCase(),
-        table = obj.className ? obj.className.toSnakeCase() : false,
-        args = [schema, table],
-        sql = FP.formatSql("update %I.%I set is_deleted = true where guid=$1;", args);
+    _patch = function (obj) {
+      var args = [obj.className ? obj.className.toSnakeCase() : false],
+       i;
 
-      plv8.execute(sql, [obj.guid]);
+      for (i = 0; i < obj.data.length; i++) {
+        switch (obj.op)
+        {
+        case "add":
+          return _add(obj);
+          break;
+        case "replace":
+          return _patch(obj);
+          break;
+        case "remove":
+          return _remove(obj);
+          break;
+        default:
+          return false;
+        }
+      }
+    };
+
+    /** private */
+    _remove = function (obj) {
+      plv8.execute("update fp.object set is_deleted = true where guid=$1;", [obj.guid]);
       
       return true;
-    }
+    };
 
     plv8._init = true;
 
