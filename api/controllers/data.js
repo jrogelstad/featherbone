0c81c352f223630b08eb395ce6dd6e92374111dd
John Rogelstad <john@rogelstad.net>
2015-06-07 18:03:33 -0400

    First tiny baby steps building API with node-swagger

---
+++ api/controllers/data.js
@@ -0,0 +1,93 @@
+'use strict';
+/*
+ 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
+ http://www.w3schools.com/js/js_strict.asp
+*/
+
+/*
+ Modules make it possible to import JavaScript files into your application.  Modules are imported
+ using 'require' statements that give you a reference to the module.
+
+  It is a good idea to list the modules that your application depends on in the package.json in the project root
+ */
+var util = require('util'),
+  pg = require("pg"),
+  conn = "postgres://postgres:password@localhost/demo",
+  client = new pg.Client(conn);
+
+/*
+ Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
+
+ For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
+
+ Either:
+  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
+  - Or the operationId associated with the operation in your Swagger document
+
+  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
+  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+ */
+module.exports = {
+  hello: hello,
+  data: data
+};
+
+/*
+  Functions in a127 controllers used for operations should take two parameters:
+
+  Param 1: a handle to the request object
+  Param 2: a handle to the response object
+ */
+function hello(req, res) {
+  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
+  var name = req.swagger.params.name.value || 'stranger';
+  var hello = util.format('Hello, %s!', name);
+
+  // this sends back a JSON response which is a single string
+  res.json(hello);
+}
+
+function data(req, res) {
+  var begin, query, qcallback;
+
+  //console.log(req);
+
+  begin = function (err) {
+    if (err) { 
+      console.error(err);
+      return;
+    }
+
+    client.connect(query);
+  }
+
+  query = function (err, resp) {
+    var payload, sql;
+
+    if (err) { 
+      console.error(err);
+      return;
+    }
+
+    payload = {
+      action: "GET",
+      name: "Contact",
+      user: "postgres"
+    };
+    sql = "SELECT request($$" + JSON.stringify(payload) + "$$);";
+
+    client.query(sql, qcallback);
+  }
+
+  qcallback = function qcallback(err, resp) {
+    if (err) { 
+      console.error(err);
+      return;
+    }
+
+    // this sends back a JSON response which is a single string
+    res.json(JSON.stringify(resp));
+  }
+
+  begin();
+}
