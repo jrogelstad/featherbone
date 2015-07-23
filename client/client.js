46f7a24858ae7a9301d3af83a522e2662127df90
jrogelstad <john@rogelstad.net>
2015-07-23 18:49:25 -0400

    Move global instantiation to core file.

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-/*global m, window */
+/*global m, f */
 
 //this application only has one component: todo
 var todo = {};
@@ -71,140 +71,6 @@ todo.view = function () {
 
 //initialize the application
 m.mount(document, {controller: todo.controller, view: todo.view});
-
-
-var State = (typeof require === 'function' ? require('statechart') :
-      window.statechart).State;
-
-var f = {
-
-  /**
-    Return a unique identifier string.
-
-    Moddified from https://github.com/google/closure-library
-    @author arv@google.com (Erik Arvidsson)
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    @return {String}
-  */
-  createId: function () {
-    var x = 2147483648,
-      dt = new Date(),
-      result = Math.floor(Math.random() * x).toString(36) +
-        Math.abs(Math.floor(Math.random() * x) ^ dt).toString(36);
-
-    return result;
-  },
-
-  /*
-    TODO: Make this real
-  */
-  getCurrentUser: function () {
-    return "admin";
-  },
-
-  /**
-    Return a date that is the current time.
-
-    @return {Date}
-  */
-  now: function () {
-    return new Date();
-  },
-
-  /**
-    Creates a property getter setter function with a default value.
-    Includes state...
-
-    @param {Any} Initial value
-    @return {Function}
-  */
-  prop: function (store) {
-    var newValue, oldValue, p, state, revert;
-
-    // Initialize state
-    state = State.define(function () {
-      this.state("Ready", function () {
-        this.event("change", function () {
-          this.goto("../Changing");
-        });
-        this.event("silence", function () {
-          this.goto("../Silent");
-        });
-        this.event("disable", function () {
-          this.goto("../Disabled");
-        });
-      });
-      this.state("Changing", function () {
-        this.event("changed", function () {
-          this.goto("../Ready");
-        });
-      });
-      this.state("Silent", function () {
-        this.event("report", function () {
-          this.goto("../Ready");
-        });
-        this.event("Disable", function () {
-          this.goto("../Disabled");
-        });
-      });
-      this.state("Disabled", function () {
-        // Attempts to make changes from disabled mode revert back
-        this.event("changed", revert);
-        this.event("enable", function () {
-          this.goto("../Ready");
-        });
-      });
-    });
-
-    revert = function () {
-      store = oldValue;
-    };
-
-    // Private function that will be returned
-    p = function (value) {
-      if (arguments.length) {
-        newValue = value;
-        oldValue = store;
-
-        p.state.send("change");
-        store = newValue;
-        p.state.send("changed");
-
-        newValue = undefined;
-        oldValue = newValue;
-      }
-
-      return store;
-    };
-
-    /*
-      Getter setter for the new value
-
-    */
-    p.newValue = function (value) {
-      if (arguments.length) {
-        newValue = value;
-      }
-
-      return newValue;
-    };
-    p.oldValue = function () {
-      return oldValue;
-    };
-
-    p.state = state;
-
-    p.toJSON = function () {
-      return store;
-    };
-
-    // Initialize state
-    state.goto();
-
-    return p;
-  }
-};
 
 f.model = function (data, my) {
   data = data || {};
@@ -386,7 +252,7 @@ f.model = function (data, my) {
     });
   };
 
-  state = State.define(function () {
+  state = f.State.define(function () {
     this.state("Ready", function () {
       this.state("New", function () {
         this.enter(doInit);
@@ -469,7 +335,7 @@ f.settings = function (name) {
     state.goto("/Busy/Saving");
   };
 
-  state = State.define(function () {
+  state = f.State.define(function () {
     this.state("Ready", function () {
       this.state("New", function () {
         this.event("fetch", doFetch);
