435076c1fb832b12ca383c15edee70eb46c08512
jrogelstad <john@rogelstad.net>
2015-09-12 14:29:00 -0400

    Start to actually build an app.

--- client/client.js
+++ client/client.js
@@ -2,7 +2,9 @@
 
 f.init().then(function () {
 
-  var ContactsWidget, ContactForm, ContactList;
+  var Home, ContactForm, ContactList,
+    ary = [],
+    idx = {};
 
   f.models.contact.list = function (data) {
     return m.request({
@@ -10,179 +12,232 @@ f.init().then(function () {
       url: "/data/contacts",
       data: data
     }).then(function (data) {
-      return data.map(function (item) {
-        return f.models.contact(item);
-      });
+      var id, model,
+        len = data.length,
+        i = 0;
+      while (i < len) {
+        id = data[i].id;
+        model = f.models.contact(data[i]);
+        if (idx[id]) {
+          ary.splice(idx[id], 1, model);
+        } else {
+          idx[id] = ary.length;
+          ary.push(model);
+        }
+        i++;
+      }
+      return ary;
     });
   };
 
-  ContactsWidget = {
-    controller: function update() {
-      this.contacts = f.models.contact.list();
-      this.save = function (contact) {
-        contact.save().then(update.bind(this));
-      }.bind(this);
-    },
-    view: function (ctrl) {
-      return [
-        m.component(ContactForm, {onsave: ctrl.save}),
-        m.component(ContactList, {contacts: ctrl.contacts})
-      ];
-    }
-  };
-
-  ContactForm = {
-    controller: function (args) {
-      this.contact = m.prop(args.contact || f.models.contact());
-    },
-    view: function (ctrl, args) {
-      var contact = ctrl.contact();
-
-      return m("form", [
-        m("label", "Name"),
-        m("input", {
-          oninput: m.withAttr("value", contact.data.name),
-          value: contact.data.name()
-        }),
-
-        m("label", "Email"),
-        m("input", {
-          oninput: m.withAttr("value", contact.data.email),
-          value: contact.data.email()
-        }),
-
-        m("button[type=button]", {
-          onclick: args.onsave.bind(this, contact)
-        }, "Save")
-      ]);
-    }
-  };
-
-  ContactList = {
-    view: function (ctrl, args) {
-      return m("table", [
-        args.contacts().map(function (contact) {
-          return m("tr", [
-            m("td", contact.data.id()),
-            m("td", contact.data.name()),
-            m("td", contact.data.email())
-          ]);
-        })
-      ]);
-    }
-  };
-
-  m.mount(document.body, ContactsWidget);
-/*
-  var Observable, ContactsWidget, ContactForm, ContactList, list;
-
-  Observable = function () {
-    var channels = {};
-    return {
-      register: function (subscriptions, controller) {
-        return function self() {
-          var ctrl = new controller,
-            reload = controller.bind(ctrl);
-          Observable.on(subscriptions, reload);
-          ctrl.onunload = function () {
-            Observable.off(reload);
-          };
-          return ctrl;
-        };
-      },
-      on: function (subscriptions, callback) {
-        subscriptions.forEach(function (subscription) {
-          if (!channels[subscription]) { channels[subscription] = []; }
-          channels[subscription].push(callback);
-        });
-      },
-      off: function (callback) {
-        var index, keys;
-        keys = Object.keys(channels);
-        keys.forEach(function (channel) {
-          index = channels[channel].indexOf(callback);
-          if (index > -1) { channels[channel].splice(index, 1); }
-        });
-      },
-      trigger: function (channel, args) {
-        console.log(channel);
-        channels[channel].map(function (callback) {
-          callback(args);
-        });
-      }
-    };
-  }.call();
-
-  //model layer observer
-  Observable.on(["saveContact"], function (model) {
-    var data = {data: model.toJSON()};
-    Contact.save(data).then(Observable.trigger("updateContact"));
-  });
-
-  ContactsWidget = {
-    controller: Observable.register(["updateContact"], function () {
-      this.contacts = Contact.list();
-    }),
-    view: function (ctrl) {
-      return [
-        m.component(ContactForm),
-        m.component(ContactList, {contacts: ctrl.contacts})
-      ];
-    }
-  };
-
-  ContactForm = {
-    controller: function (args) {
-      var that = this;
-      this.contact = f.models.contact();
-      this.save = function () {
-        Observable.trigger("saveContact", that.contact);
+  Home = {
+    controller: function () {
+      this.doContact = function () {
+        m.route("/contact");
+      };
+      this.doContacts = function () {
+        m.route("/contacts");
       };
     },
-    view: function (ctrl, args) {
-      var contact = ctrl.contact,
+    view: function (ctrl) {
+      return m("div", [
+        m("button[type=button]", {
+          onclick: ctrl.doContacts
+        }, "Contacts")
+      ]);
+    }
+  };
+
+  ContactForm = {
+    controller: function () {
+      this.contact = m.prop(f.models.contact());
+      this.doApply = function (contact) {
+        contact.save();
+      };
+      this.doSave = function (contact) {
+        contact.save().then(function () {
+          m.route("/contacts");
+        });
+      };
+      this.doContacts = function () {
+        m.route("/contacts");
+      };
+    },
+    view: function (ctrl) {
+      var contact = ctrl.contact(),
         d = contact.data;
 
       return m("form", [
-        m("label", "Name"),
-        m("input", {
-          oninput: m.withAttr("value", d.name),
-          value: d.name()
-        }),
-
-        m("label", "Email"),
-        m("input", {
-          oninput: m.withAttr("value", d.email),
-          value: d.email()
-        }),
-
         m("button[type=button]", {
-          onclick: ctrl.save
-        }, "Save")
+          onclick: ctrl.doContacts
+        }, "Done"),
+        m("button[type=button]", {
+          onclick: ctrl.doApply.bind(this, contact)
+        }, "Apply"),
+        m("button[type=button]", {
+          onclick: ctrl.doSave.bind(this, contact)
+        }, "Save"),
+        m("table", [
+          m("tr", [
+            m("td", [
+              m("label", {for: "name"}, "Name:")
+            ]),
+            m("td", [
+              m("input", {
+                id: "name",
+                type: "text",
+                required: true,
+                autofocus: true,
+                oninput: m.withAttr("value", d.name),
+                value: d.name()
+              })
+            ]),
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", {for: "email"}, "Email:")
+            ]),
+            m("td", [
+              m("input", {
+                id: "email",
+                type: "email",
+                oninput: m.withAttr("value", d.email),
+                value: d.email()
+              })
+            ])
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", {for: "isActive"}, "Active:")
+            ]),
+            m("td", [
+              m("input", {
+                id: "isActive",
+                type: "checkbox",
+                oninput: m.withAttr("value", d.isActive),
+                value: d.isActive()
+              })
+            ])
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", "Created:")
+            ]),
+            m("td", [
+              m("input", {
+                type: "datetime",
+                readonly: true,
+                oninput: m.withAttr("value", d.created),
+                value: d.created()
+              })
+            ]),
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", "Created By:")
+            ]),
+            m("td", [
+              m("input", {
+                type: "text",
+                readonly: true,
+                oninput: m.withAttr("value", d.createdBy),
+                value: d.createdBy()
+              })
+            ]),
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", "Updated:")
+            ]),
+            m("td", [
+              m("input", {
+                type: "datetime",
+                readonly: true,
+                oninput: m.withAttr("value", d.updated),
+                value: d.updated()
+              })
+            ])
+          ]),
+          m("tr", [
+            m("td", [
+              m("label", "Updated By:")
+            ]),
+            m("td", [
+              m("input", {
+                type: "text",
+                readonly: true,
+                oninput: m.withAttr("value", d.updatedBy),
+                value: d.updatedBy()
+              })
+            ])
+          ])
+        ])
       ]);
     }
   };
 
   ContactList = {
-    view: function (ctrl, args) {
-      return m("table", [
-        args.contacts().map(function (contact) {
-          var model = f.models.contact(contact),
-            d = model.data;
-          return m("tr", [
-            m("td", d.id()),
-            m("td", d.name()),
-            m("td", d.email())
-          ]);
-        })
+    controller: function () {
+      var selections = [];
+      this.contacts = f.models.contact.list();
+      this.doHome = function () {
+        m.route("/home");
+      };
+      this.doContact = function () {
+        m.route("/contact");
+      };
+      this.toggleSelect = function (id) {
+        var sel = selections.indexOf(id);
+        if (sel === -1) {
+          selections.push(id);
+          return true;
+        }
+        selections.splice(sel, 1);
+        return false;
+      };
+      this.selected = function (id) {
+        return selections.indexOf(id) !== -1;
+      };
+    },
+    view: function (ctrl) {
+      return m("div", [
+        m("button[type=button]", {
+          onclick: ctrl.doHome
+        }, "Home"),
+        m("button[type=button]", {
+          onclick: ctrl.doContact
+        }, "New"),
+        m("table", [
+          m("tr", [
+            m("th", "Id"),
+            m("th", "Name"),
+            m("th", "Email")
+          ]),
+          ctrl.contacts().map(function (contact) {
+            var d = contact.data,
+              id = d.id();
+            return m("tr", {
+              onclick: ctrl.toggleSelect.bind(this, id),
+              style: {
+                backgroundColor: ctrl.selected(id) ? "LightBlue" : "White"
+              }
+            }, [
+              m("td", d.id()),
+              m("td", d.name()),
+              m("td", d.email())
+            ]);
+          })
+        ])
       ]);
     }
   };
 
-  m.mount(document.body, ContactsWidget);
-*/
+  m.route(document.body, "/home", {
+    "/home": Home,
+    "/contacts": ContactList,
+    "/contact": ContactForm
+  });
 });
-
 
 window.onresize = function (event) {
   m.redraw(true);
