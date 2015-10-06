2522dac513b338d97864408a667895ff905f656a
jrogelstad <john@rogelstad.net>
2015-10-05 20:40:24 -0400

    First pass building app dynamically

--- client/client.js
+++ client/client.js
@@ -2,42 +2,75 @@
 
 f.init().then(function () {
 
-  var Home, ContactFormDisplay, ContactTableDisplay;
+  var config, keys,
+    app = {},
+    routes = {};
 
-  Home = {
-    controller: function () {
-      this.goContacts = function () {
-        m.route("/contacts");
-      };
-    },
-    view: function (ctrl) {
-      return m("div", [
-        m("button[type=button]", {
-          onclick: ctrl.goContacts
-        }, "Contacts")
-      ]);
+  config = {
+    Contact: {
+      form: {
+        attrs: ["name", "isActive", "email", "birthDate", "workPhone",
+          "homePhone", "annualIncome", "creditScore", "role", "created",
+          "createdBy", "updated", "updatedBy"]
+      },
+      list: {
+        attrs: ["name", "email", "isActive", "birthDate", "workPhone",
+          "homePhone", "creditScore"]
+      }
     }
   };
 
-  ContactFormDisplay = f.components.formDisplay({
-    feather: "Contact",
-    attrs: ["name", "isActive", "email", "birthDate", "workPhone",
-      "homePhone", "annualIncome", "creditScore", "role", "created",
-      "createdBy", "updated", "updatedBy"]
+  keys = Object.keys(config);
+
+  // Build home navigation page
+  app.Home = {
+    controller: function () {
+      var that = this;
+
+      keys.forEach(function (key) {
+        var plural = f.catalog.getFeather(key).plural;
+
+        that["go" + plural] = function () {
+          m.route("/" + plural.toSpinalCase());
+        };
+      });
+    },
+    view: function (ctrl) {
+      var buttons = keys.map(function (key) {
+          var plural = f.catalog.getFeather(key).plural;
+
+          return m("button[type=button]", {
+            onclick: ctrl["go" + plural]
+          }, plural);
+        });
+      return m("div", buttons);
+    }
+  };
+  routes["/home"] = app.Home;
+
+  // Build app for each configured object
+  keys.forEach(function (key) {
+    var plural = f.catalog.data()[key].plural.toSpinalCase(),
+      name = key.toSpinalCase();
+
+    // Build UI
+    app[key + "TableDisplay"] = f.components.tableDisplay({
+      feather: key,
+      attrs: config[key].list.attrs
+    });
+
+    app[key + "FormDisplay"] = f.components.formDisplay({
+      feather: key,
+      attrs: config[key].form.attrs
+    });
+
+    // Build routes
+    routes["/" + plural] = app[key + "TableDisplay"];
+    routes["/" + name] = app[key + "FormDisplay"];
+    routes["/" + name + "/:id"] = app[key + "FormDisplay"];
   });
 
-  ContactTableDisplay = f.components.tableDisplay({
-    feather: "Contact",
-    attrs: ["name", "email", "isActive", "birthDate", "workPhone",
-      "homePhone", "creditScore"]
-  });
-
-  m.route(document.body, "/home", {
-    "/home": Home,
-    "/contacts": ContactTableDisplay,
-    "/contact": ContactFormDisplay,
-    "/contact/:id": ContactFormDisplay
-  });
+  m.route(document.body, "/home", routes);
 });
 
 window.onresize = function (event) {
