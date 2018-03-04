ebf4cc7947100a7ae0a28d47c9f1935cb553b6b5
John Rogelstad <jrogelstad@users.noreply.github.com>
2018-03-04 18:06:22 -0500

    Update upsert to handle promises

--- pg/controllers.js
+++ pg/controllers.js
@@ -5,38 +5,29 @@
   var f = require("./common/core");
 
   var doUpsertTable = function (obj) {
-    var afterSaveFeather,
-      client = obj.client,
-      callback = obj.callback,
-      table = obj.data,
-      feather = f.copy(table);
+    return new Promise (function (resolve, reject) {
+      var table = obj.data,
+        feather = f.copy(table);
 
-    afterSaveFeather = function (err, ignore) {
-      if (err) {
-        callback(err);
-        return;
-      }
+      // Save the table as a feather in the catalog
+      var props = feather.properties;
+      feather.properties = {};
+      props.forEach(function (prop) {
+        feather.properties[prop.name] = prop;
+        delete prop.name;
+      });
 
-      callback(null, obj);
-    };
-
-    // Save the table as a feather in the catalog
-    var props = feather.properties;
-    feather.properties = {};
-    props.forEach(function (prop) {
-      feather.properties[prop.name] = prop;
-      delete prop.name;
+      datasource.request({
+        method: "PUT",
+        name: "saveFeather",
+        data: {
+          specs: feather
+        },
+        client: obj.client
+      }, true)
+      .then(resolve)
+      .catch(reject);
     });
-
-    datasource.request({
-      method: "PUT",
-      name: "saveFeather",
-      data: {
-        specs: feather
-      },
-      client: client,
-      callback: afterSaveFeather
-    }, true);
   };
 
   datasource.registerFunction("POST", "Table", doUpsertTable, datasource.TRIGGER_BEFORE);
