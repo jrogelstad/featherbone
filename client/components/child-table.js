66ce4a6d40fa7281c9cf0aabff84ec5f25d01bda
John Rogelstad <john@rogelstad.net>
2018-10-30 07:00:21 +0800

    Lint

--- client/components/child-table.js
+++ client/components/child-table.js
@@ -15,217 +15,240 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-
+/*global require, module*/
+/*jslint this*/
 (function () {
-  "use strict";
+    "use strict";
 
-  var findRoot,
-    childTable = {},
-    m = require("mithril"),
-    stream = require("stream"),
-    button = require("button"),
-    catalog = require("catalog"),
-    tableWidget = require("table-widget");
+    var findRoot,
+        childTable = {},
+        m = require("mithril"),
+        stream = require("stream"),
+        button = require("button"),
+        catalog = require("catalog"),
+        tableWidget = require("table-widget");
 
-  findRoot = function (model) {
-    var parent,
-      d = model.data,
-      keys = Object.keys(d);
+    findRoot = function (model) {
+        var parent,
+            d = model.data,
+            keys = Object.keys(d);
 
-    parent = keys.find(function (key) {
-      if (d[key].isChild()) { return true; }
-    });
+        parent = keys.find(function (key) {
+            if (d[key].isChild()) {
+                return true;
+            }
+        });
 
-    return parent ? findRoot(d[parent]()) : model;
-  };
-
-  /**
-    View model for child table.
-
-    @param {Object} Options
-    @param {Array} [options.models] Array of child models
-    @param {String} [options.feather] Feather
-    @param {Array} [options.config] Column configuration
-  */
-  childTable.viewModel = function (options) {
-    var tableState,
-      root = findRoot(options.parentViewModel.model()),
-      vm = {};
-
-    // ..........................................................
-    // PUBLIC
-    //
-
-    vm.buttonAdd = stream();
-    vm.buttonOpen = stream();
-    vm.buttonRemove = stream();
-    vm.buttonUndo = stream();
-    //vm.formDialog = stream();
-    vm.tableWidget = stream();
-    vm.parentViewModel = stream(options.parentViewModel);
-    vm.refresh = function () {
-      vm.tableWidget().refresh();
+        return parent
+            ? findRoot(d[parent]())
+            : model;
     };
 
-    // ..........................................................
-    // PRIVATE
-    //
+    /**
+      View model for child table.
 
-    // Create dalog view model
-    /*
-    vm.formDialog(formDialog.viewModel({
-      filter: vm.tableWidget().filter,
-      list: vm.tableWidget().models(),
-      feather: feather
-    }));
+      @param {Object} Options
+      @param {Array} [options.models] Array of child models
+      @param {String} [options.feather] Feather
+      @param {Array} [options.config] Column configuration
     */
+    childTable.viewModel = function (options) {
+        var tableState,
+            root = findRoot(options.parentViewModel.model()),
+            vm = {};
 
-    // Create table widget view model
-    vm.tableWidget(tableWidget.viewModel({
-      config: options.config,
-      models: options.models,
-      feather: options.feather,
-      containerId: vm.parentViewModel().containerId()
-      //ondblclick: vm.formDialog().show
-    }));
-    vm.tableWidget().toggleEdit();
-    vm.tableWidget().isQuery(false);
+        // ..........................................................
+        // PUBLIC
+        //
 
-    // Create button view models
-    vm.buttonAdd(button.viewModel({
-      onclick: vm.tableWidget().modelNew,
-      title: "Insert",
-      hotkey: "I",
-      icon: "plus-circle",
-      style: {backgroundColor: "white"}
-    }));
-
-    vm.buttonRemove(button.viewModel({
-      onclick: vm.tableWidget().modelDelete,
-      title: "Delete",
-      hotkey: "D",
-      icon: "remove",
-      style: {backgroundColor: "white"}
-    }));
-    vm.buttonRemove().disable();
-
-    vm.buttonUndo(button.viewModel({
-      onclick: vm.tableWidget().undo,
-      title: "Undo",
-      hotkey: "U",
-      icon: "undo",
-      style: {backgroundColor: "white"}
-    }));
-    vm.buttonUndo().hide();
-
-    vm.buttonOpen(button.viewModel({
-      //onclick: vm.formDialog().show,
-      title: "Open",
-      hotkey: "O",
-      icon: "folder-open",
-      style: { backgroundColor: "white" }
-    }));
-    vm.buttonOpen().disable();
-
-    // Bind buttons to table widget state change events
-    tableState = vm.tableWidget().state();
-    tableState.resolve("/Selection/Off").enter(function () {
-      vm.buttonOpen().disable();
-      vm.buttonRemove().disable();
-      vm.buttonRemove().show();
-      vm.buttonUndo().hide();
-    });
-    tableState.resolve("/Selection/On").enter(function () {
-        var current = root.state().current()[0];
-        if (current !== "/Ready/Fetched/ReadOnly" && current !== "/Locked") {
-            //vm.buttonOpen().enable();
-            vm.buttonRemove().enable();
-        }
-    });
-    tableState.resolve("/Selection/On/Clean").enter(function () {
-      vm.buttonRemove().show();
-      vm.buttonUndo().hide();
-    });
-    tableState.resolve("/Selection/On/Dirty").enter(function () {
-      vm.buttonRemove().hide();
-      vm.buttonUndo().show();
-    });
-    root.state().resolve("/Ready/Fetched/Clean").enter(function () {
-      var selection = vm.tableWidget().selection(),
-        found = function (model) {
-          return selection.id() === model.id();
+        vm.buttonAdd = stream();
+        vm.buttonOpen = stream();
+        vm.buttonRemove = stream();
+        vm.buttonUndo = stream();
+        //vm.formDialog = stream();
+        vm.tableWidget = stream();
+        vm.parentViewModel = stream(options.parentViewModel);
+        vm.refresh = function () {
+            vm.tableWidget().refresh();
         };
-      // Unselect potentially deleted model
-      if (selection && !vm.tableWidget().models().some(found)) {
-        vm.tableWidget().select(undefined);
-      }
-    });
-    root.state().resolve("/Ready/Fetched/ReadOnly").enter(vm.buttonAdd().disable)
-    root.state().resolve("/Ready/Fetched/ReadOnly").exit(vm.buttonAdd().enable);
-    root.state().resolve("/Locked").enter(vm.buttonAdd().disable);
-    root.state().resolve("/Locked").exit(vm.buttonAdd().enable);
 
-    return vm;
-  };
+        // ..........................................................
+        // PRIVATE
+        //
 
-  /**
-    Child table component
+        // Create dalog view model
+        /*
+        vm.formDialog(formDialog.viewModel({
+          filter: vm.tableWidget().filter,
+          list: vm.tableWidget().models(),
+          feather: feather
+        }));
+        */
 
-    @params {Object} View model
-  */
-  childTable.component = {
-    oninit: function (vnode) {
-      var config, overload, keys, parentState,
-        parentProperty = vnode.attrs.parentProperty,
-        parentViewModel = vnode.attrs.parentViewModel,
-        prop = parentViewModel.model().data[parentProperty],
-        models = prop(),
-        feather = catalog.getFeather(prop.type.relation),
-        parentFeather = catalog.getFeather(parentViewModel.model().name),
-        overloads = parentFeather.overloads || {},
-        relations = vnode.attrs.parentViewModel.relations();
+        // Create table widget view model
+        vm.tableWidget(tableWidget.viewModel({
+            config: options.config,
+            models: options.models,
+            feather: options.feather,
+            containerId: vm.parentViewModel().containerId()
+                //ondblclick: vm.formDialog().show
+        }));
+        vm.tableWidget().toggleEdit();
+        vm.tableWidget().isQuery(false);
 
-      config = parentViewModel.config().attrs.find(function (item)  {
-        return item.attr === parentProperty;
-      });
+        // Create button view models
+        vm.buttonAdd(button.viewModel({
+            onclick: vm.tableWidget().modelNew,
+            title: "Insert",
+            hotkey: "I",
+            icon: "plus-circle",
+            style: {
+                backgroundColor: "white"
+            }
+        }));
 
-      // Apply parent defined overloads to child feather
-      overload = overloads[parentProperty];
-      if (overload) {
-        feather.overloads = feather.overloads || {};
-        keys = Object.keys(overload);
-        keys.forEach(function (key) {
-          feather.overloads[key] = overload[key];
+        vm.buttonRemove(button.viewModel({
+            onclick: vm.tableWidget().modelDelete,
+            title: "Delete",
+            hotkey: "D",
+            icon: "remove",
+            style: {
+                backgroundColor: "white"
+            }
+        }));
+        vm.buttonRemove().disable();
+
+        vm.buttonUndo(button.viewModel({
+            onclick: vm.tableWidget().undo,
+            title: "Undo",
+            hotkey: "U",
+            icon: "undo",
+            style: {
+                backgroundColor: "white"
+            }
+        }));
+        vm.buttonUndo().hide();
+
+        vm.buttonOpen(button.viewModel({
+            //onclick: vm.formDialog().show,
+            title: "Open",
+            hotkey: "O",
+            icon: "folder-open",
+            style: {
+                backgroundColor: "white"
+            }
+        }));
+        vm.buttonOpen().disable();
+
+        // Bind buttons to table widget state change events
+        tableState = vm.tableWidget().state();
+        tableState.resolve("/Selection/Off").enter(function () {
+            vm.buttonOpen().disable();
+            vm.buttonRemove().disable();
+            vm.buttonRemove().show();
+            vm.buttonUndo().hide();
         });
-      }
-
-      // Set up viewModel if required
-      if (!relations[parentProperty]) {
-        relations[parentProperty] = childTable.viewModel({
-          parentViewModel: parentViewModel,
-          models: models,
-          feather: feather,
-          config: config
+        tableState.resolve("/Selection/On").enter(function () {
+            var current = root.state().current()[0];
+            if (current !== "/Ready/Fetched/ReadOnly" && current !== "/Locked") {
+                //vm.buttonOpen().enable();
+                vm.buttonRemove().enable();
+            }
         });
-      }
-      this.viewModel = relations[parentProperty];
-    },
+        tableState.resolve("/Selection/On/Clean").enter(function () {
+            vm.buttonRemove().show();
+            vm.buttonUndo().hide();
+        });
+        tableState.resolve("/Selection/On/Dirty").enter(function () {
+            vm.buttonRemove().hide();
+            vm.buttonUndo().show();
+        });
+        root.state().resolve("/Ready/Fetched/Clean").enter(function () {
+            var selection = vm.tableWidget().selection(),
+                found = function (model) {
+                    return selection.id() === model.id();
+                };
+            // Unselect potentially deleted model
+            if (selection && !vm.tableWidget().models().some(found)) {
+                vm.tableWidget().select(undefined);
+            }
+        });
+        root.state().resolve("/Ready/Fetched/ReadOnly").enter(vm.buttonAdd().disable);
+        root.state().resolve("/Ready/Fetched/ReadOnly").exit(vm.buttonAdd().enable);
+        root.state().resolve("/Locked").enter(vm.buttonAdd().disable);
+        root.state().resolve("/Locked").exit(vm.buttonAdd().enable);
 
-    view: function () {
-      var vm = this.viewModel;
+        return vm;
+    };
 
-      return m("div", [
-        m(button.component, {viewModel: vm.buttonAdd()}),
-        m(button.component, {viewModel: vm.buttonRemove()}),
-        m(button.component, {viewModel: vm.buttonUndo()}),
-        m(button.component, {viewModel: vm.buttonOpen()}),
-        m(tableWidget.component, {viewModel: vm.tableWidget()})
-      ]);
-    }
-  };
+    /**
+      Child table component
 
-  catalog.register("components", "childTable", childTable.component);
+      @params {Object} View model
+    */
+    childTable.component = {
+        oninit: function (vnode) {
+            var config, overload, keys,
+                    parentProperty = vnode.attrs.parentProperty,
+                    parentViewModel = vnode.attrs.parentViewModel,
+                    prop = parentViewModel.model().data[parentProperty],
+                    models = prop(),
+                    feather = catalog.getFeather(prop.type.relation),
+                    parentFeather = catalog.getFeather(parentViewModel.model().name),
+                    overloads = parentFeather.overloads || {},
+                    relations = vnode.attrs.parentViewModel.relations();
 
-  module.exports = childTable;
+            config = parentViewModel.config().attrs.find(function (item) {
+                return item.attr === parentProperty;
+            });
+
+            // Apply parent defined overloads to child feather
+            overload = overloads[parentProperty];
+            if (overload) {
+                feather.overloads = feather.overloads || {};
+                keys = Object.keys(overload);
+                keys.forEach(function (key) {
+                    feather.overloads[key] = overload[key];
+                });
+            }
+
+            // Set up viewModel if required
+            if (!relations[parentProperty]) {
+                relations[parentProperty] = childTable.viewModel({
+                    parentViewModel: parentViewModel,
+                    models: models,
+                    feather: feather,
+                    config: config
+                });
+            }
+            this.viewModel = relations[parentProperty];
+        },
+
+        view: function () {
+            var vm = this.viewModel;
+
+            return m("div", [
+                m(button.component, {
+                    viewModel: vm.buttonAdd()
+                }),
+                m(button.component, {
+                    viewModel: vm.buttonRemove()
+                }),
+                m(button.component, {
+                    viewModel: vm.buttonUndo()
+                }),
+                m(button.component, {
+                    viewModel: vm.buttonOpen()
+                }),
+                m(tableWidget.component, {
+                    viewModel: vm.tableWidget()
+                })
+            ]);
+        }
+    };
+
+    catalog.register("components", "childTable", childTable.component);
+
+    module.exports = childTable;
 
 }());
