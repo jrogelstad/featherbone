2da195a4bb42605841d6267fb1a22afe7cffe639
John Rogelstad <john@rogelstad.net>
2018-12-29 14:36:58 -0500

    Lint

--- client/components/form-dialog.js
+++ client/components/form-dialog.js
@@ -1,6 +1,6 @@
 /**
     Framework for building object relational database apps
-    Copyright (C) 2018  John Rogelstad
+    Copyright (C) 2019  John Rogelstad
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -15,102 +15,118 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-
+/*global require, module*/
+/*jslint browser*/
 (function () {
-  "use strict";
+    "use strict";
 
-  var formDialog = {},
-    m = require("mithril"),
-    stream = require("stream"),
-    dialog = require("dialog"),
-    formWidget = require("form-widget");
+    const formDialog = {};
+    const m = require("mithril");
+    const stream = require("stream");
+    const dialog = require("dialog");
+    const formWidget = require("form-widget");
 
-  /**
-    View model for form dialog.
+    /**
+      View model for form dialog.
 
-    @param {Object} Options
-  */
-  formDialog.viewModel = function (options) {
-    var vm, substate,
-      onOk = options.onOk;
+      @param {Object} Options
+    */
+    formDialog.viewModel = function (options) {
+        let vm;
+        let substate;
+        let onOk = options.onOk;
 
-    // ..........................................................
-    // PUBLIC
-    //
+        // ..........................................................
+        // PUBLIC
+        //
 
-    options.title = options.title || options.model.toName();
-    options.icon = options.icon || "file-text";
-    options.onOk = function () {
-      var model = vm.formWidget().model();
-      model.save().then(function () {
-        if (onOk) { onOk(model); }
-      });
+        options.title = options.title || options.model.toName();
+        options.icon = options.icon || "file-text";
+        options.onOk = function () {
+            let model = vm.formWidget().model();
+            model.save().then(function () {
+                if (onOk) {
+                    onOk(model);
+                }
+            });
+        };
+
+        vm = dialog.viewModel(options);
+        vm.formWidget = stream();
+        vm.modelId = stream(options.id);
+        vm.okDisabled = function () {
+            let w = vm.formWidget();
+            return (
+                w
+                ? !w.model().canSave()
+                : true
+            );
+        };
+        vm.okTitle = function () {
+            let w = vm.formWidget();
+            let def = "Record is unchanged";
+            return (
+                w
+                ? w.model().lastError() || def
+                : ""
+            );
+        };
+        vm.content = function () {
+            let state = vm.state();
+            return state.resolve(state.current()[0]).content();
+        };
+
+        // ..........................................................
+        // PRIVATE
+        //
+
+        // Only create the form instance when showing. Otherwise leads to
+        // creating forms
+        // for entire relation tree which is too heavy and could lead to
+        // infinite loops
+        substate = vm.state().resolve("/Display/Closed");
+        substate.content = function () {
+            return m("div");
+        };
+        substate = vm.state().resolve("/Display/Showing");
+        substate.enter(function () {
+            // Create dalog view models
+            vm.formWidget(formWidget.viewModel({
+                model: options.model,
+                config: options.config,
+                id: vm.modelId(),
+                outsideElementIds: [
+                    vm.ids().header,
+                    vm.ids().buttonOk
+                ],
+                containerId: vm.ids().dialog
+            }));
+            delete vm.style().display;
+        });
+        substate.content = function () {
+            return m(formWidget.component, {
+                viewModel: vm.formWidget()
+            });
+        };
+        substate.exit(function () {
+            vm.formWidget(undefined);
+            vm.style().display = "none";
+        });
+
+        delete vm.style().width;
+        vm.style().margin = "25px";
+        vm.style().top = "0px";
+        vm.style().display = "none";
+
+        return vm;
     };
 
-    vm = dialog.viewModel(options);
-    vm.formWidget = stream();
-    vm.modelId = stream(options.id);
-    vm.okDisabled = function () {
-      var w = vm.formWidget();
-      return w ? !w.model().canSave() : true;
-    };
-    vm.okTitle = function () {
-      var w = vm.formWidget(),
-        def = "Record is unchanged";
-      return w ? w.model().lastError() ||  def : "";
-    };
-    vm.content = function () {
-      var state = vm.state();
-      return state.resolve(state.current()[0]).content();
-    };
+    /**
+      Form dialog component
 
-    // ..........................................................
-    // PRIVATE
-    //
-
-    // Only create the form instance when showing. Otherwise leads to creating forms
-    // for entire relation tree which is too heavy and could lead to infinite loops
-    substate = vm.state().resolve("/Display/Closed");
-    substate.content = function () {
-      return m("div"); 
-    };
-    substate = vm.state().resolve("/Display/Showing");
-    substate.enter(function () {
-      // Create dalog view models
-      vm.formWidget(formWidget.viewModel({
-        model: options.model,
-        config: options.config,
-        id: vm.modelId(),
-        outsideElementIds: [
-          vm.ids().header,
-          vm.ids().buttonOk
-        ],
-        containerId: vm.ids().dialog
-      }));
-      delete vm.style().display;
-    });
-    substate.content = function () {
-      return  m(formWidget.component, {viewModel: vm.formWidget()});
-    };
-    substate.exit(function () {
-      vm.formWidget(undefined);
-      vm.style().display = "none";
-    });
-
-    vm.style().width = undefined;
-    vm.style().margin = "25px";
-    vm.style().top = "0px";
-    vm.style().display = "none";
-
-    return vm;
-  };
-
-  /**
-    Form dialog component
-
-    @params {Object} View model
-  */
-  formDialog.component = dialog.component;
-  module.exports = formDialog;
+      @params {Object} View model
+    */
+    formDialog.component = dialog.component;
+    module.exports = formDialog;
 
 }());
