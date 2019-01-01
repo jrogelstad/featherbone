8ef36efdd4e04abed91931842dc8f77e0588fa3e
John Rogelstad <john@rogelstad.net>
2019-01-01 10:21:45 -0500

    Lint

--- client/core.js
+++ client/core.js
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-/*jslint browser*/
+/*jslint this, browser*/
 import {catalog} from "./models/catalog.js";
 import {stream} from "../common/stream.js";
 import {State} from "../common/state.js";
--- scripts/module.js
+++ scripts/module.js
@@ -19,5 +19,6 @@
     "use strict";
 
     // Placeholder
+    return;
 
 }());
