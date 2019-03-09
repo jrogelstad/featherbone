df2c157cee6bee23396c83c480ac03dc3baf6d57
John Rogelstad <john@rogelstad.net>
2019-03-09 06:21:47 -0500

    Consolidate
    worksheet dialog is not a resuable component

--- client/components/workbook-page.js
+++ client/components/workbook-page.js
@@ -22,13 +22,410 @@ import catalog from "../models/catalog.js";
 import dialog from "./dialog.js";
 import filterDialog from "./filter-dialog.js";
 import sortDialog from "./sort-dialog.js";
-import sheetConfigureDialog from "./sheet-configure-dialog.js";
 import searchInput from "./search-input.js";
 import tableWidget from "./table-widget.js";
 import navigator from "./navigator-menu.js";
+import tableDialog from "./table-dialog.js";
+import checkbox from "./checkbox.js";
 
 const workbookPage = {};
+const sheetConfigureDialog = {};
 const m = window.m;
+
+// View model for worksheet configuration.
+sheetConfigureDialog.viewModel = function (options) {
+    options = options || {};
+    let vm;
+    let tableView;
+    let createModel = catalog.store().models().workbookLocalConfig;
+    let cache = f.copy(options.parentViewModel.sheet());
+    let sheetButtonClass;
+    let listButtonClass;
+    let sheetTabClass;
+    let listTabClass;
+
+    options.onOk = function () {
+        let id = vm.sheetId();
+        let sheet = vm.model().toJSON();
+        let tableWidget = options.parentViewModel.tableWidget();
+
+        vm.sheet(id, sheet);
+        // If we updated current sheet (not new), update list
+        if (vm.sheet().id === id) {
+            tableWidget.config(sheet.list);
+        }
+        vm.state().send("close");
+    };
+    options.icon = "gear";
+    options.title = "Configure worksheet";
+
+    // ..........................................................
+    // PUBLIC
+    //
+
+    vm = tableDialog.viewModel(options);
+    tableView = vm.content;
+    vm.alias = function (attr) {
+        let feather = catalog.getFeather(vm.model().data.feather());
+
+        return f.resolveAlias(feather, attr);
+    };
+    vm.addAttr = function (attr) {
+        if (!this.some(vm.hasAttr.bind(attr))) {
+            this.push({
+                attr: attr
+            });
+            return true;
+        }
+    };
+    vm.attrs = function () {
+        let model = vm.model();
+        let feather = catalog.getFeather(model.data.feather());
+        let keys = (
+            feather
+            ? Object.keys(feather.properties)
+            : false
+        );
+
+        return (
+            keys
+            ? vm.resolveProperties(feather, keys).sort()
+            : []
+        );
+    };
+    vm.config = options.parentViewModel.config;
+    vm.content = function () {
+        let feathers;
+        let forms;
+        let d = vm.model().data;
+        let ids = vm.ids();
+        let nameId = ids.name;
+        let featherId = ids.feather;
+        let openInNewWindowId = ids.openInNewWindow;
+        let formId = ids.form;
+
+        feathers = vm.feathers().map(function (feather) {
+            return m("option", feather);
+        });
+
+        forms = vm.forms().map(function (form) {
+            return m("option", form);
+        });
+
+        return m("div", {
+            class: "pure-form pure-form-aligned fb-sheet-configure-content"
+        }, [
+            m("div", {
+                class: "fb-sheet-configure-tabs"
+            }, [
+                m("button", {
+                    class: sheetButtonClass,
+                    style: {
+                        borderRadius: "4px 0px 0px 4px"
+                    },
+                    onclick: vm.toggleTab
+                }, "Sheet"),
+                m("button", {
+                    class: listButtonClass,
+                    style: {
+                        borderRadius: "0px 4px 4px 0px"
+                    },
+                    onclick: vm.toggleTab
+                }, "Columns")
+            ]),
+            m("div", {
+                class: "fb-sheet-configure-group-box"
+            }, [
+                m("div", {
+                    class: sheetTabClass
+                }, [
+                    m("div", {
+                        class: "pure-control-group"
+                    }, [
+                        m("label", {
+                            for: nameId
+                        }, "Name:"),
+                        m("input", {
+                            id: nameId,
+                            value: d.name(),
+                            required: true,
+                            oninput: (e) => d.name(e.target.value)
+                        })
+                    ]),
+                    m("div", {
+                        class: "pure-control-group"
+                    }, [
+                        m("label", {
+                            for: featherId
+                        }, "Feather:"),
+                        m("select", {
+                            id: featherId,
+                            value: d.feather(),
+                            required: true,
+                            oninput: (e) => d.feather(e.target.value)
+                        }, feathers)
+                    ]),
+                    m("div", {
+                        class: "pure-control-group"
+                    }, [
+                        m("label", {
+                            for: openInNewWindowId
+                        }, "Open in New Tab:"),
+                        m(checkbox.component, {
+                            id: openInNewWindowId,
+                            value: d.openInNewWindow(),
+                            onclick: d.openInNewWindow
+                        })
+                    ]),
+                    m("div", {
+                        class: "pure-control-group"
+                    }, [
+                        m("label", {
+                            for: formId
+                        }, "Form:"),
+                        m("select", {
+                            id: formId,
+                            value: vm.form(),
+                            required: true,
+                            oninput: (e) => vm.form(e.target.value),
+                            disabled: forms.length === 0,
+                            style: {
+                                minWidth: "215px"
+                            }
+                        }, forms)
+                    ])
+                ]),
+                m("div", {
+                    class: listTabClass
+                }, [
+                    tableView()
+                ])
+            ])
+        ]);
+    };
+    vm.data = function () {
+        return vm.model().data.list().data.columns();
+    };
+    vm.hasAttr = function (item) {
+        return item.attr === this;
+    };
+    vm.feathers = function () {
+        let feathers = catalog.store().feathers();
+        let result = Object.keys(feathers).filter(function (name) {
+            return !feathers[name].isChild && !feathers[name].isSystem;
+        }).sort();
+
+        return result;
+    };
+    vm.form = function (...args) {
+        let forms;
+        let form;
+        let name = args[0];
+        let store = catalog.store();
+        let prop = vm.model().data.form;
+
+        if (args.length) {
+            forms = store.data().forms();
+            form = forms.find(function (row) {
+                return row.name === name;
+            });
+            prop(store.models().form(form));
+        }
+        return (
+            prop()
+            ? prop().data.name()
+            : ""
+        );
+    };
+    vm.forms = function () {
+        let result;
+        let forms = catalog.store().data().forms();
+        let feather = vm.model().data.feather();
+
+        // Only forms that have matching feather
+        result = forms.filter(function (form) {
+            return form.feather === feather;
+        });
+
+        // Just return names
+        result = result.map(function (form) {
+            return form.name;
+        }).sort();
+
+        return result;
+    };
+    vm.model = f.prop();
+    vm.okDisabled = function () {
+        return !vm.model().isValid();
+    };
+    vm.okTitle = function () {
+        return vm.model().lastError();
+    };
+    vm.sheetId = f.prop(options.sheetId);
+    vm.relations = f.prop({});
+    vm.reset = function () {
+        let model;
+        let id = vm.sheetId();
+
+        // Setup local model
+        cache = f.copy(vm.sheet(id));
+        model = createModel(cache);
+        model.onChanged("form", m.redraw);
+        vm.model(model);
+        if (!cache.list.columns.length) {
+            vm.add();
+        }
+        vm.selection(0);
+
+        // Set button orientation
+        sheetButtonClass = "pure-button pure-button-primary";
+        listButtonClass = "pure-button";
+        sheetTabClass = "";
+        listTabClass = "fb-tabbed-panes-hidden";
+    };
+    vm.resolveProperties = function (feather, properties, ary, prefix) {
+        prefix = prefix || "";
+        let result = ary || [];
+
+        properties.forEach(function (key) {
+            let rfeather;
+            let prop = feather.properties[key];
+            let isObject = typeof prop.type === "object";
+            let path = prefix + key;
+
+            if (isObject && prop.type.properties) {
+                rfeather = catalog.getFeather(prop.type.relation);
+                vm.resolveProperties(
+                    rfeather,
+                    prop.type.properties,
+                    result,
+                    path + "."
+                );
+            }
+
+            if (
+                isObject && (
+                    prop.type.childOf ||
+                    prop.type.parentOf ||
+                    prop.type.isChild
+                )
+            ) {
+                return;
+            }
+
+            result.push(path);
+        });
+
+        return result;
+    };
+    vm.sheet = options.parentViewModel.sheet;
+    vm.toggleTab = function () {
+        if (sheetTabClass) {
+            sheetButtonClass = "pure-button pure-button-primary";
+            listButtonClass = "pure-button";
+            sheetTabClass = "";
+            listTabClass = "fb-tabbed-panes-hidden";
+        } else {
+            sheetButtonClass = "pure-button";
+            listButtonClass = "pure-button pure-button-primary";
+            sheetTabClass = "fb-tabbed-panes-hidden";
+            listTabClass = "";
+        }
+    };
+    vm.workbook = options.parentViewModel.workbook;
+    vm.viewHeaderIds = f.prop({
+        column: f.createId(),
+        label: f.createId()
+    });
+    vm.viewHeaders = function () {
+        let ids = vm.viewHeaderIds();
+
+        return [
+            m("th", {
+                style: {
+                    minWidth: "165px"
+                },
+                id: ids.column
+            }, "Column"),
+            m("th", {
+                style: {
+                    minWidth: "220px"
+                },
+                id: ids.label
+            }, "Label")
+        ];
+    };
+    vm.viewRows = function () {
+        let view;
+
+        view = vm.items().map(function (item) {
+            let row;
+
+            row = m("tr", {
+                onclick: vm.selection.bind(this, item.index, true),
+                style: {
+                    backgroundColor: vm.rowColor(item.index)
+                }
+            }, [
+                m("td", {
+                    style: {
+                        minWidth: "165px",
+                        maxWidth: "165px"
+                    }
+                }, m("select", {
+                    id: "scdSelect" + item.index,
+                    value: item.attr,
+                    style: {
+                        maxWidth: "165px"
+                    },
+                    onchange: (e) =>
+                    vm.itemChanged.bind(
+                        this,
+                        item.index,
+                        "attr"
+                    )(e.target.value)
+                }, vm.attrs().map(function (attr) {
+                    return m("option", {
+                        value: attr
+                    }, attr.toName());
+                }))),
+                m("td", {
+                    style: {
+                        minWidth: "220px",
+                        maxWidth: "220px"
+                    }
+                }, m("input", {
+                    value: item.label || vm.alias(item.attr),
+                    onchange: (e) =>
+                    vm.itemChanged.bind(
+                        this,
+                        item.index,
+                        "label"
+                    )(e.target.value)
+                }))
+            ]);
+
+            return row;
+        });
+
+        return view;
+    };
+
+    // ..........................................................
+    // PRIVATE
+    //
+
+    vm.ids().name = f.createId();
+    vm.ids().feather = f.createId();
+    vm.ids().openInNewWindow = f.createId();
+    vm.ids().form = f.createId();
+    vm.style().width = "510px";
+    vm.reset();
+
+    return vm;
+};
+
+sheetConfigureDialog.component = tableDialog.component;
 
 // Define workbook view model
 workbookPage.viewModel = function (options) {
