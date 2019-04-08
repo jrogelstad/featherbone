78956945b73a89d16792fd4e26eebe47f7b74c56
John Rogelstad <john@rogelstad.net>
2019-04-08 21:28:15 +0200

    Trap

---
+++ client/models/role.js
@@ -0,0 +1,35 @@
+/**
+    Framework for building object relational database apps
+    Copyright (C) 2019  John Rogelstad
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+import catalog from "./catalog.js";
+import model from "./model.js";
+
+/*
+  Role Model
+*/
+function role(data, feather) {
+    feather = feather || catalog.getFeather("Role");
+    let that = model(data, feather);
+
+    that.state().resolve("/Ready/Fetched/Clean").enter(function () {
+        that.data.name.isReadOnly(true);
+    });
+
+    return that;
+}
+
+catalog.registerModel("Role", role, true);
