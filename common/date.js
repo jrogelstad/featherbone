d91979c75e54005571b838a5d08f6f08089868d7
John Rogelstad <john@rogelstad.net>
2020-04-25 15:27:34 -0400

    Update date.js
    Add get week helper function

--- common/date.js
+++ common/date.js
@@ -79,4 +79,31 @@
         return this;
     };
 
+    /**
+       Return the week number of the date.
+
+       From: https://weeknumber.net/how-to/javascript
+
+       @method getWeek
+       @return {number}
+    */
+    Date.prototype.getWeek = function () {
+        let date = new Date(this.getTime());
+        let week1;
+
+        date.setHours(0, 0, 0, 0);
+        // Thursday in current week decides the year.
+        date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+        // January 4 is always in week 1.
+        week1 = new Date(date.getFullYear(), 0, 4);
+        // Adjust to Thursday in week 1 and count number of weeks
+        // from date to week1.
+        return 1 + Math.round(
+            (
+                (
+                    date.getTime() - week1.getTime()
+                ) / 86400000 - 3 + (week1.getDay() + 6) % 7
+            ) / 7
+        );
+    };
 }());
