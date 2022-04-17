732e7d6eb1621a06c4a7cdb5fc2fda5d27ce1076
John Rogelstad <john@rogelstad.net>
2022-04-17 10:07:07 -0400

    Linting

--- client/models/catalog.js
+++ client/models/catalog.js
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/*jslint this, browser*/
+/*jslint this, browser, unordered*/
 /**
     @module Catalog
 */
