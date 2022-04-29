8cee0e87d15736918249d37ba15ca4bd391a36a5
John Rogelstad <john@rogelstad.net>
2022-04-29 17:56:52 -0400

    Lint

--- client/models/module.js
+++ client/models/module.js
@@ -1,6 +1,6 @@
 /*
     Framework for building object relational database apps
-    Copyright (C) 2021  John Rogelstad
+    Copyright (C) 2022  John Rogelstad
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-/*jslint browser*/
+/*jslint browser unordered*/
 /*global f*/
 
 function module(data, feather) {
