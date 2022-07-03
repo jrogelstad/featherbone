51fc5179e61cdad59c3f9003f28ff35c9e8084e5
Stephen Cote <sw.cote@gmail.com>
2022-07-03 13:51:02 -0500

    Add a parse limit for JSON

--- server.js
+++ server.js
@@ -1426,7 +1426,7 @@
         app.use(bodyParser.urlencoded({
             extended: true
         }));
-        app.use(bodyParser.json());
+        app.use(bodyParser.json({limit: '5mb'}));
 
         // Set up authentication with passport
         passport.use(new LocalStrategy(
