0814c17e23dda65788e0e79687c6a06443e0c19b
John Rogelstad <john@rogelstad.net>
2023-08-15 07:07:05 -0400

    Remove dead code

--- client/components/workbook-page.js
+++ client/components/workbook-page.js
@@ -100,11 +100,7 @@ const editSheetConfig = {
         attr: "helpLink",
         grid: 1,
         label: "Help Page"
-    }/*, { This works, but overkill?
-        attr: "isClearOnNoSearch",
-        label: "Clear on no search",
-        grid: 1
-    }*/, {
+    }, {
         attr: "columns",
         showLabel: false,
         height: "208px",
@@ -352,7 +348,6 @@ workbookPage.viewModel = function (options) {
             feather: sheet.feather,
             form: sheet.form || "D",
             drawerForm: sheet.drawerForm || "N",
-            isClearOnNoSearch: Boolean(sheet.isClearOnNoSearch),
             isEditModeEnabled: sheet.isEditModeEnabled,
             openInNewWindow: sheet.openInNewWindow,
             actions: sheet.actions || [],
@@ -370,7 +365,6 @@ workbookPage.viewModel = function (options) {
             sheet.feather = data.feather;
             sheet.form = data.form;
             sheet.drawerForm = data.drawerForm;
-            sheet.isClearOnNoSearch = data.isClearOnNoSearch;
             sheet.isEditModeEnabled = data.isEditModeEnabled;
             sheet.openInNewWindow = data.openInNewWindow;
             sheet.list.columns.length = 0;
@@ -406,7 +400,6 @@ workbookPage.viewModel = function (options) {
             } else {
                 vm.buttonEdit().disable();
             }
-            vm.tableWidget().isClearOnNoSearch(data.isClearOnNoSearch);
             vm.tableWidget().isEditModeEnabled(data.isEditModeEnabled);
             handleDrawer();
 
