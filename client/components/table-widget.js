66bcac67cc76dee447d27d3cc89590ae156c41f8
John Rogelstad <john@rogelstad.net>
2024-08-16 16:29:34 -0400

    Fix labels
    Fix filter labels for money and relation attributes

--- client/components/table-widget.js
+++ client/components/table-widget.js
@@ -478,6 +478,7 @@ function createTableHeader(options, col) {
     let key = col.attr;
     let icon = [];
     let fidx;
+    let fval;
     let operators = f.operators;
     let columnWidth = (
         config.columns[options.idx].width || COL_WIDTH_DEFAULT
@@ -545,11 +546,15 @@ function createTableHeader(options, col) {
     // Add filter icons
     fidx = findFilterIndex(key);
     if (fidx !== false) {
+        fval = filter.criteria[fidx].value;
+        if (typeof fval === "object") {
+            fval = fval.naturalKey;
+        }
         icon.push(m("i", {
             class: "material-icons-outlined fb-column-filter-icon",
             title: operators[
                 (filter.criteria[fidx].operator || "=")
-            ] + " " + filter.criteria[fidx].value,
+            ] + " \"" + fval + "\"",
             style: {
                 fontSize: vm.zoom() * 0.80 + "%"
             }
