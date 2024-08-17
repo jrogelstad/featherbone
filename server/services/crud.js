2a58b1f96523a21bea6ad6975cc628bc6b12459a
John Rogelstad <john@rogelstad.net>
2024-08-17 09:37:04 -0400

    Release lock on delete
    Release lock on delete, and also update with latest user and time.

--- server/services/crud.js
+++ server/services/crud.js
@@ -972,7 +972,12 @@
                 let afterDoSelect;
                 let afterDelete;
                 let afterLog;
-                let sql = "UPDATE object SET is_deleted = true WHERE id=$1;";
+                let sql = (
+                    "UPDATE object SET is_deleted = true, " +
+                    "  lock = null, updated = now(), updated_by = $2 " +
+                    "WHERE id=$1;"
+                );
+                let params = [obj.id, obj.client.currentUser()];
                 let sql2 = "DELETE from \"$subscription\" WHERE objectid=$1;";
                 let clen = 1;
                 let c = 0;
@@ -980,6 +985,7 @@
 
                 if (obj.isHard === true) {
                     sql = "DELETE FROM object WHERE id=$1;";
+                    params = [obj.id];
                 }
 
                 noChildProps = function (key) {
@@ -1083,7 +1089,7 @@
 
                     // Finally, delete parent object
                     await theClient.query(sql2, [obj.id]);
-                    theClient.query(sql, [obj.id], afterDelete);
+                    theClient.query(sql, params, afterDelete);
                 };
 
                 // Handle change log
@@ -1097,29 +1103,6 @@
                     await theClient.query(sql2, [obj.id]);
                     afterLog();
                     return;
-
-                    // Won't get here as code above prevents it
-                    // TODO add flag in feather to make optional
-                    /*
-                    if (isChild || obj.isHard) {
-                        afterLog();
-                        return;
-                    }
-
-                    // Log the completed deletion
-                    crud.doInsert({
-                        name: "Log",
-                        data: {
-                            objectId: obj.id,
-                            action: "DELETE",
-                            created: now,
-                            createdBy: now,
-                            updated: now,
-                            updatedBy: now
-                        },
-                        client: theClient
-                    }, true).then(afterLog).catch(reject);
-                    */
                 };
 
                 afterLog = function () {
